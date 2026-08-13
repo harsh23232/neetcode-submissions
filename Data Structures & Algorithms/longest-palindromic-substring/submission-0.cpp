90b1229273e603734db89d74480698070a45854f
Harsh Pal <147902951+harsh23232@users.noreply.github.com>
2026-08-13 16:51:14 +0530

    Add: longest-palindromic-substring - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+
+    bool ispal(int l , int r , string s ){
+        while (l<=r){
+            if (s[l]==s[r]){
+                l++;
+                r--;
+            }
+            else{
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+    string longestPalindrome(string s) {
+
+        int i = 0;
+
+        int j = s.size()-1;
+
+        int cnt = 0;
+
+        int maxs = 1;
+
+        string st ;
+
+        while (i < s.length()){
+
+            if( s[i]==s[j]){
+                if (i==j){
+
+                    i++;
+                    j= s.size()-1;
+
+                }
+                else if (ispal(i,j,s)){
+                    cnt=j-i+1;
+
+                    if (cnt >= maxs){
+                        maxs= cnt;
+                        st = s.substr(i,cnt);
+                    }
+                    i++;
+                    j= s.size()-1;
+                    
+
+                }
+                else {
+                    j--;
+
+                }
+                
+            }
+            else{
+                j--;
+            }
+
+            
+            
+
+            
+        }
+
+        return st;
+    
+        
+    }
+};
