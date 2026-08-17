7797a453a0c94ca502ce1c8da8956d959241e167
Harsh Pal <147902951+harsh23232@users.noreply.github.com>
2026-08-17 19:41:32 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        vector<int>freq(26,0);
+        for(char c :s){
+            freq[c-'a']++;
+        }
+        for(char c :t){
+            freq[c -'a']--;
+        }
+        for(int i=0;i<freq.size();i++){
+            if(freq[i] !=0){
+                return false;
+            }
+        }return true;
+        
+    }
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        int n = strs.size();
+        vector<bool>vis(n,false);
+
+        for(int i = 0; i < n; i++){
+            if (vis[i]) continue;
+            vector<string> group;
+            group.push_back(strs[i]);
+            vis[i] = true;
+        
+  
+        
+            for(int j =i+1;j<n;j++){
+                if(!vis[j] && isAnagram(strs[i],strs[j])){
+                    group.push_back(strs[j]);
+                    vis[j] = true;
+                }
+            }ans.push_back(group);
+        }
+        return ans;
+        
+    }
+};
